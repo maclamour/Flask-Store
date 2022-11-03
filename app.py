02073d5b0bd31f20880ac2d67d3dd50bd10f689b
maclamour <maclamour@gmail.com>
2022-11-03 09:28:04 -0400

    route tested in Postman

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ stores = [
     'items': [
         {
             'name': 'Chair',
-            'price': 10.00
+            'price': 10.99
+        },
+        {
+            'name': 'Rug',
+            'price': 100.67
         }
     ],
 
