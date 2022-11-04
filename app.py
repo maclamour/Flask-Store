6fa3bb8e91a1bf7766463c945399cc26e3941691
maclamour <maclamour@gmail.com>
2022-11-04 08:18:15 -0400

    create Post route for new store items

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask,request
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
+
 stores = [
     {'name': 'My Store',
     'items': [
@@ -30,4 +29,16 @@ def create_store():
     new_store = {'name': request_data['name'],'items':[]}
     stores.append(new_store)
     return new_store, 201 
+
+@app.post('/store/<string.name>/item')
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {'name': request_data['name'], 'price': request_data['price']}
+            store['items'].append(new_item)
+            return new_item, 201
+    return {'message': 'Error creating item, Store not found'},404
+
+
 
