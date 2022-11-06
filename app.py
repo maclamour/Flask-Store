532860dd51e084134e46e4f555573d090317bc0e
maclamour <maclamour@gmail.com>
2022-11-06 08:11:15 -0500

    delete store route created, tested in Postman, passed

--- app.py
+++ app.py
@@ -91,6 +91,14 @@ def update_item(item_id):
     except KeyError:
         abort(404, message='Item not found.')
 
+@app.delete('/store/<string:store_id>')
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": 'Store deleted.'}
+    except KeyError:
+        abort(404, message='Store not found.')
+
 
 
     
