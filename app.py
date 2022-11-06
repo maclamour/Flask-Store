a497c6a90680c3d77b46b72711dd6712ea8f57cd
maclamour <maclamour@gmail.com>
2022-11-06 07:50:47 -0500

    added delete route in flask app

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def create_item():
             400,
             message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload.",
         )
-    for item in item.values():
+    for item in items.values():
         if (
             item_data ['name'] == item['name']
             and item_data['store_id'] == item['store_id']
@@ -56,10 +56,6 @@ def create_item():
     items[item_id] = item
     return item, 201 
 
-@app.get('/item')
-def get_item():
-    return {'stores':list(items.values())}
-
 @app.get('/store/<string:store_id>')
 def get_store(store_id):
     try:
@@ -68,9 +64,20 @@ def get_store(store_id):
         abort(404, message='Store not found.')
 
 @app.get('/item/<string:item_id>')
-def get_item_in_store(item_id):
+def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
         abort(404, message='Item not found.')
+
+@app.delete('/item/<string:item_id>')
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": 'Item deleted.'}
+    except KeyError:
+        abort(404, message='Item not found.')
     
+@app.get('/item')
+def get_all_items():
+    return {'stores':list(items.values())}
