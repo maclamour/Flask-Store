ef5cfbe68304b3a79fe776d388e5b6836a52e1a8
maclamour <maclamour@gmail.com>
2022-11-06 06:51:57 -0500

    cleaned up app file to help reference store and items in a dict and index key vaule pairs in the database

---
+++ db.py
@@ -0,0 +1,16 @@
+stores = {}
+items = {
+    1: {
+        'name': 'Chair',
+        'price':300.10
+    },
+    2: {
+        'name': 'Bookcase',
+        'price':800.20
+    },
+    3: {
+        'name': 'Lamp',
+        'price':70.99
+    }
+
+}
