f2a6725b208366dbc5e6f9d9fca176c7c638a29e
maclamour <maclamour@gmail.com>
2022-11-07 07:59:46 -0500

    tested all blueprint routes in Postman for both stores and items, passed

--- resources/item.py
+++ resources/item.py
@@ -13,24 +13,27 @@ class Item(MethodView):
         except KeyError:
             abort(404, message='Item not found.') 
 
-    def delete(item_id):
+    def delete(self,item_id):
         try:
             del items[item_id]
             return {"message": 'Item deleted.'}
         except KeyError:
             abort(404, message='Item not found.')
 
-    def put(item_id):
+    def put(self, item_id):
         item_data = request.get_json()
-        if 'price' not in item_data or 'name' not in item_data:
-            abort(400,message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.",)
-
+        if "price" not in item_data or "name" not in item_data:
+            abort(
+                400,
+                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+            )
         try:
             item = items[item_id]
             item |= item_data
-            return item 
+            return item
+        
         except KeyError:
-            abort(404, message='Item not found.')
+            abort(404, message="Item not found.")
 
 @blp.route('/item')
 class ItemList(MethodView):
