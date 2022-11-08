e2d749e40d679c6a98f2ed39e66f88329f6ba523
maclamour <maclamour@gmail.com>
2022-11-08 07:14:29 -0500

    installed SQLalchemy and flask-sqlalchemy, set up base models for items and stores

--- db.py
+++ db.py
@@ -1,16 +1,5 @@
-stores = {}
-items = {
-    1: {
-        'name': 'Chair',
-        'price':300.10
-    },
-    2: {
-        'name': 'Bookcase',
-        'price':800.20
-    },
-    3: {
-        'name': 'Lamp',
-        'price':70.99
-    }
+from flack_sqlalchemy import SQLAlchemy
 
-}
+db = SQLAlchemy()
+
+
