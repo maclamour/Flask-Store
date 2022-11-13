169f8a45360e76703280b11f60e324ee2a562f9e
maclamour <maclamour@gmail.com>
2022-11-13 18:17:46 -0500

    updated schema

--- schemas.py
+++ schemas.py
@@ -32,3 +32,8 @@ class ItemUpdateSchema(Schema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
